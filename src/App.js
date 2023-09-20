ac719839b12faf16cacf1705b991f2b204d269e8
Venkat Ratnam Sabbavarapu <venkatratnamsabbavarapu@Venkats-MacBook-Air.local>
2023-09-20 16:20:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+function Square({value})
+{
+  function handleClick(){
+    console.log("Clicked!");
+  }
+  return (<button className="square" onClick={handleClick}>{value}</button>);
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="board-row">
+      <Square value="1"/>
+      <Square value="2"/>
+      <Square value="3"/>
     </div>
+    <div className="board-row">
+    <Square value="4"/>
+      <Square value="5"/>
+      <Square value="6"/>
+    </div>
+    <div className="board-row">
+    <Square value="7"/>
+      <Square value="8"/>
+      <Square value="9"/>
+    </div>
+    </>
+    
   );
 }
 
