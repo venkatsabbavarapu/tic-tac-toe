587c224777044959d39dc2fcceab69dd05e9c1e2
Venkat Ratnam Sabbavarapu <venkatratnamsabbavarapu@Venkats-MacBook-Air.local>
2023-09-21 23:16:51 -0700

    added state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-function Square({value})
+import { useState } from "react";
+function Square()
 {
+  const [value, setValue]=useState(null);
   function handleClick(){
-    console.log("Clicked!");
+    setValue('X');
   }
   return (<button className="square" onClick={handleClick}>{value}</button>);
 }
@@ -9,19 +11,19 @@ function App() {
   return (
     <>
     <div className="board-row">
-      <Square value="1"/>
-      <Square value="2"/>
-      <Square value="3"/>
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-    <Square value="4"/>
-      <Square value="5"/>
-      <Square value="6"/>
+    <Square />
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-    <Square value="7"/>
-      <Square value="8"/>
-      <Square value="9"/>
+    <Square />
+      <Square />
+      <Square />
     </div>
     </>
     
