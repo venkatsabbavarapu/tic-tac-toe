c7306e54c1d3b5b07ca4fdd5ae30a382034467d4
Venkat Ratnam Sabbavarapu <venkatratnamsabbavarapu@Venkats-MacBook-Air.local>
2023-09-22 23:49:07 -0700

    Working game

--- src/App.js
+++ src/App.js
@@ -1,33 +1,80 @@
 import { useState } from "react";
-function Square()
+function Square({value, onSquareClick})
 {
-  const [value, setValue]=useState(null);
-  function handleClick(){
-    setValue('X');
-  }
-  return (<button className="square" onClick={handleClick}>{value}</button>);
+  return (<button className="square" onClick={onSquareClick}>{value}</button>);
 }
-function App() {
+function Board() {
+  const [xIsNext, setXIsNext]=useState(true);
+  const [squares, setSquares]=useState(Array(9).fill(null));
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  function handleClick(i){
+    if(squares[i] || calculateWinner(squares))
+    {
+      return;
+    }
+    const nextSquares=squares.slice();
+    if(xIsNext)
+    {
+    nextSquares[i]="X";
+    setXIsNext(false);
+    }
+    else
+    {
+      nextSquares[i]="O";
+      setXIsNext(true);
+    }
+    setSquares(nextSquares);
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   return (
     <>
+    <div className="status">{status}</div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
     </div>
     <div className="board-row">
-    <Square />
-      <Square />
-      <Square />
+    <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
     </div>
     <div className="board-row">
-    <Square />
-      <Square />
-      <Square />
+    <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
     </>
     
   );
 }
 
-export default App;
+export default Board;
